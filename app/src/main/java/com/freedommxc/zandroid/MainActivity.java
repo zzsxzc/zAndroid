3ad00746f64717d0371f28df3f6c40d538c5032d
zhengzhaosheng <1350111595@qq.com>
2021-01-04 20:56:12 +0800

    Initial commit

---
+++ app/src/main/java/com/freedommxc/zandroid/MainActivity.java
@@ -0,0 +1,18 @@
+package com.freedommxc.zandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+*@Author zheng zs
+*@Date 20:51 2021/1/4
+*@Description 本工程用于android代码复用。
+**/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
