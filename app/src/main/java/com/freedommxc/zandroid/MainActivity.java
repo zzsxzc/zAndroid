93839f70666bbc0da815064edf7bd51a3c915865
zhengzhaosheng <1350111595@qq.com>
2021-01-04 21:00:30 +0800

    test

--- app/src/main/java/com/freedommxc/zandroid/MainActivity.java
+++ app/src/main/java/com/freedommxc/zandroid/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 /**
 *@Author zheng zs
 *@Date 20:51 2021/1/4
-*@Description 本工程用于android代码复用。
+*@Description 本工程用于android代码复用哦。
 **/
 public class MainActivity extends AppCompatActivity {
 
